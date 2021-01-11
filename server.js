c6eafa604f4c489abd592a1c72d2db8ef729c111
Lawson Kelly <wlawsonkelly@gmail.com>
2021-01-11 13:02:04 -0500

    heroku

--- server.js
+++ server.js
@@ -15,7 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
